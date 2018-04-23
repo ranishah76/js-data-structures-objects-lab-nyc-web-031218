6c30efb4858798c72dcfc0fba0a5187bae2cee6b
Rani Shah <rani.shah@flatironschool.com>
2018-04-23 16:22:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+ var driver = { name: 'Fiat'};
+
+ function updateDriverWithKeyAndValue(driver, key, value) {
+    newdriver = {...driver}
+    newdriver[key] = value
+    return newdriver
+ }
+
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value
+    return driver
+ }
+
+function deleteFromDriverByKey(driver, key) {
+    newdriver = Object.assign({}, driver);
+    delete newdriver[key]
+    return newdriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
